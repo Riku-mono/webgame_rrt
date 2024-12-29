19fcea1c6f412c17048b255b21e2040516aeee24
Riku-mono <riitkou@gmail.com>
2024-12-29 21:50:20 +0900

    add account app and custome user model

--- webgame_rrt/settings.py
+++ webgame_rrt/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'online.apps.OnlineConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -135,3 +136,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
