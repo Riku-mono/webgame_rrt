034fa3e8ad1d7727e8d65f29354f4784291074f4
Riku-mono <riitkou@gmail.com>
2024-12-30 00:24:37 +0900

    add bootstrap5 support and configure static files

--- webgame_rrt/settings.py
+++ webgame_rrt/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_bootstrap5',
     'online.apps.OnlineConfig',
     'accounts.apps.AccountsConfig',
 ]
@@ -131,6 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
